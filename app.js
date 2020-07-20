1db9791a05d99ebc421d9e3d4058f1d25872f1e3
newhar <gtqh2yj@gmail.com>
2020-07-20 15:28:58 +0900

    Refactoring
    It's done!

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ let canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("control__color");
 const range = document.getElementById("jsRange");
+const mode = document.getElementById("js_fill");
+const savebtn = document.getElementById("js_save");
 
 canvas.width = 400;
 canvas.height = 400;
 
 // 초기 선 상태 등록
 let painting = false;
+let filling = false;
 
 ctx.lineWidth = 2.5;
 ctx.strokeStyle = "black";
+ctx.fillStyle = "black";
 
 function startPainting() {
   painting = true;
@@ -35,23 +39,68 @@ function onMouseMove(event) {
 
 // 색 변환
 function changeColorHandler(event) {
-  ctx.strokeStyle = event.target.style.backgroundColor;
-  //   console.log(event.target.style.backgroundColor);
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+  ctx.fillStyle = color;
+}
+
+function modeHandler() {
+  if (filling === true) {
+    filling = false;
+    mode.innerHTML = "paint";
+  } else {
+    filling = true;
+    mode.innerHTML = "fill";
+  }
+}
+
+function fillingHandler() {
+  if (filling) {
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+}
+
+// 파일 저장
+function saveHandler() {
+  const link = document.createElement("a");
+  const img = canvas.toDataURL();
+  link.href = img;
+  link.download = "paintJS";
+  link.click();
 }
 
 // 브러쉬 두께 조절
 function changeBrushHandler(event) {
-  //   console.log(jsRange.value);
   ctx.lineWidth = jsRange.value;
 }
 
-canvas.addEventListener("mousemove", onMouseMove);
-canvas.addEventListener("mousedown", startPainting);
-canvas.addEventListener("mouseup", stopPainting);
-canvas.addEventListener("mouseleave", stopPainting);
+function contextmenuHandler(event) {
+  event.preventDefault();
+}
 
-Array.from(colors).forEach((color) =>
-  color.addEventListener("click", changeColorHandler)
-);
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", fillingHandler);
+  canvas.addEventListener("contextmenu", contextmenuHandler);
+}
 
-jsRange.addEventListener("mouseup", changeBrushHandler);
+if (jsRange) {
+  jsRange.addEventListener("mouseup", changeBrushHandler);
+}
+
+if (mode) {
+  mode.addEventListener("click", modeHandler);
+}
+
+if (savebtn) {
+  savebtn.addEventListener("click", saveHandler);
+}
+
+if (colors) {
+  Array.from(colors).forEach((color) =>
+    color.addEventListener("click", changeColorHandler)
+  );
+}
